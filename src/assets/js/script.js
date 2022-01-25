6fb0b43822ef6496b26e660688d7dc28443587ba
Demian <learc47@gmail.com>
2022-01-25 02:45:08 -0300

    fix ejercicio2

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,8 @@
-// Variables
+//Ejercicio n°2
 
-alert("¡Hola! Ingresa dos números diferentes y mayores a cero");
-const num1 = prompt("Ingrese el primer número");
-const num2 = prompt("Ingrese el segundo número");
+alert("¡Hola! Ingresa dos números diferentes y mayores de cero");
+const num1 = prompt("Ingresa el primer número");
+const num2 = prompt("Ingresa el segundo número");
 
 let sum = parseInt(num1) + parseInt(num2);
 let subtraction = parseInt(num1) - parseInt(num2);
@@ -14,7 +14,7 @@ let module = parseInt(num1) % parseInt(num2);
 
 if (parseInt(num1) === 0 || parseInt(num2) === 0 || num1 === num2) {
   alert(
-    "Lo siento, ambos números deben ser mayores a cero y no pueden ser iguales :)"
+    "Ambos números deben ser mayores a cero y no pueden ser iguales"
   );
 } else {
   document.write(`La suma entre ${num1} y ${num2} es: ${sum}<br>`);
@@ -24,4 +24,13 @@ if (parseInt(num1) === 0 || parseInt(num2) === 0 || num1 === num2) {
   );
   document.write(`La división entre ${num1} y ${num2} es: ${division}<br>`);
   document.write(`El módulo entre ${num1} y ${num2} es: ${module}<br>`);
+}
+
+//Ejercicio n°3
+
+function convert (){
+  let celsius = document.getElementById("celsius").value;
+  let fahrenheit = (parseInt(celsius)*(9/5)+32).toFixed(2);
+  let kelvin = (parseInt(celsius)+273.15).toFixed(2);
+  
 }
